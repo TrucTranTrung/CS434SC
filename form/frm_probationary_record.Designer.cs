e22ea7b7c36e294e58661386ec7b919fd95248d7
PikaChu307 <minhhoangnguyen1682003@gmail.com>
2024-07-06 09:30:47 +0700

    Add files via upload

---
+++ form/frm_probationary_record.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace HR_Department_Project.GUI
+{
+    partial class frm_probationary_record
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.combox_identification_number = new System.Windows.Forms.ComboBox();
+            this.combo_department = new System.Windows.Forms.ComboBox();
+            this.txt_exprity_date = new System.Windows.Forms.DateTimePicker();
+            this.txt_probationary_date = new System.Windows.Forms.DateTimePicker();
+            this.txt_note = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_exit = new System.Windows.Forms.Button();
+            this.btn_delete = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.btn_create = new System.Windows.Forms.Button();
+            this.btn_refresh = new System.Windows.Forms.Button();
+            this.cb_pos = new System.Windows.Forms.ComboBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cb_pos);
+            this.groupBox1.Controls.Add(this.combox_identification_number);
+            this.groupBox1.Controls.Add(this.combo_department);
+            this.groupBox1.Controls.Add(this.txt_exprity_date);
+            this.groupBox1.Controls.Add(this.txt_probationary_date);
+            this.groupBox1.Controls.Add(this.txt_note);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(1202, 281);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Probationary record";
+            // 
+            // combox_identification_number
+            // 
+            this.combox_identification_number.FormattingEnabled = true;
+            this.combox_identification_number.Location = new System.Drawing.Point(168, 74);
+            this.combox_identification_number.Margin = new System.Windows.Forms.Padding(4);
+            this.combox_identification_number.Name = "combox_identification_number";
+            this.combox_identification_number.Size = new System.Drawing.Size(220, 24);
+            this.combox_identification_number.TabIndex = 19;
+            // 
+            // combo_department
+            // 
+            this.combo_department.FormattingEnabled = true;
+            this.combo_department.Location = new System.Drawing.Point(528, 132);
+            this.combo_department.Margin = new System.Windows.Forms.Padding(4);
+            this.combo_department.Name = "combo_department";
+            this.combo_department.Size = new System.Drawing.Size(648, 24);
+            this.combo_department.TabIndex = 17;
+            // 
+            // txt_exprity_date
+            // 
+            this.txt_exprity_date.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.txt_exprity_date.Location = new System.Drawing.Point(528, 197);
+            this.txt_exprity_date.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_exprity_date.Name = "txt_exprity_date";
+            this.txt_exprity_date.Size = new System.Drawing.Size(648, 22);
+            this.txt_exprity_date.TabIndex = 16;
+            // 
+            // txt_probationary_date
+            // 
+            this.txt_probationary_date.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.txt_probationary_date.Location = new System.Drawing.Point(168, 197);
+            this.txt_probationary_date.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_probationary_date.Name = "txt_probationary_date";
+            this.txt_probationary_date.Size = new System.Drawing.Size(220, 22);
+            this.txt_probationary_date.TabIndex = 15;
+            // 
+            // txt_note
+            // 
+            this.txt_note.Location = new System.Drawing.Point(526, 74);
+            this.txt_note.Margin = new System.Windows.Forms.Padding(4);
+            this.txt_note.Name = "txt_note";
+            this.txt_note.Size = new System.Drawing.Size(650, 24);
+            this.txt_note.TabIndex = 14;
+            this.txt_note.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(415, 74);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Note";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(9, 81);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(151, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Identiofication_number";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(415, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(82, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Department";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(415, 202);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Expiry date ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 202);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Probationary date";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 138);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Probationary posiotion";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Controls.Add(this.btn_exit);
+            this.groupBox2.Controls.Add(this.btn_delete);
+            this.groupBox2.Controls.Add(this.btn_update);
+            this.groupBox2.Controls.Add(this.btn_create);
+            this.groupBox2.Controls.Add(this.btn_refresh);
+            this.groupBox2.Location = new System.Drawing.Point(16, 390);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(1202, 306);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Function";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 84);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1202, 214);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            // 
+            // btn_exit
+            // 
+            this.btn_exit.Location = new System.Drawing.Point(1076, 39);
+            this.btn_exit.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_exit.Name = "btn_exit";
+            this.btn_exit.Size = new System.Drawing.Size(100, 28);
+            this.btn_exit.TabIndex = 4;
+            this.btn_exit.Text = "Exit";
+            this.btn_exit.UseVisualStyleBackColor = true;
+            this.btn_exit.Click += new System.EventHandler(this.btn_exit_Click);
+            // 
+            // btn_delete
+            // 
+            this.btn_delete.Location = new System.Drawing.Point(855, 39);
+            this.btn_delete.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_delete.Name = "btn_delete";
+            this.btn_delete.Size = new System.Drawing.Size(100, 28);
+            this.btn_delete.TabIndex = 3;
+            this.btn_delete.Text = "Delete";
+            this.btn_delete.UseVisualStyleBackColor = true;
+            this.btn_delete.Click += new System.EventHandler(this.btn_delete_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(627, 39);
+            this.btn_update.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(100, 28);
+            this.btn_update.TabIndex = 2;
+            this.btn_update.Text = "Update";
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // btn_create
+            // 
+            this.btn_create.Location = new System.Drawing.Point(397, 39);
+            this.btn_create.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_create.Name = "btn_create";
+            this.btn_create.Size = new System.Drawing.Size(100, 28);
+            this.btn_create.TabIndex = 1;
+            this.btn_create.Text = "Create";
+            this.btn_create.UseVisualStyleBackColor = true;
+            this.btn_create.Click += new System.EventHandler(this.btn_create_Click);
+            // 
+            // btn_refresh
+            // 
+            this.btn_refresh.Location = new System.Drawing.Point(168, 39);
+            this.btn_refresh.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_refresh.Name = "btn_refresh";
+            this.btn_refresh.Size = new System.Drawing.Size(100, 28);
+            this.btn_refresh.TabIndex = 0;
+            this.btn_refresh.Text = "Refresh";
+            this.btn_refresh.UseVisualStyleBackColor = true;
+            this.btn_refresh.Click += new System.EventHandler(this.btn_refresh_Click);
+            // 
+            // cb_pos
+            // 
+            this.cb_pos.FormattingEnabled = true;
+            this.cb_pos.Location = new System.Drawing.Point(168, 132);
+            this.cb_pos.Margin = new System.Windows.Forms.Padding(4);
+            this.cb_pos.Name = "cb_pos";
+            this.cb_pos.Size = new System.Drawing.Size(220, 24);
+            this.cb_pos.TabIndex = 20;
+            // 
+            // frm_probationary_record
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1229, 694);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frm_probationary_record";
+            this.Text = "frm_probationary_record";
+            this.Load += new System.EventHandler(this.frm_probationary_record_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.GroupBox groupBox1;
+        public System.Windows.Forms.GroupBox groupBox2;
+        public System.Windows.Forms.DataGridView dataGridView1;
+        public System.Windows.Forms.Label label6;
+        public System.Windows.Forms.Label label4;
+        public System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.ComboBox combox_identification_number;
+        public System.Windows.Forms.ComboBox combo_department;
+        public System.Windows.Forms.DateTimePicker txt_exprity_date;
+        public System.Windows.Forms.DateTimePicker txt_probationary_date;
+        public System.Windows.Forms.RichTextBox txt_note;
+        public System.Windows.Forms.Label label7;
+        public System.Windows.Forms.Button btn_exit;
+        public System.Windows.Forms.Button btn_delete;
+        public System.Windows.Forms.Button btn_update;
+        public System.Windows.Forms.Button btn_create;
+        public System.Windows.Forms.Button btn_refresh;
+        public System.Windows.Forms.ComboBox cb_pos;
+    }
+}
---
+++ form/frm_probationary_record.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HR_Department_Project.GUI
+{
+    public partial class frm_probationary_record : Form
+    {
+        BLL.BLLProbationaryRecord bll_cn;
+        private ConnectDatabase commonClass;
+        public frm_probationary_record()
+        {
+            InitializeComponent();
+            commonClass = new ConnectDatabase();
+            bll_cn = new BLL.BLLProbationaryRecord(this);
+        }
+        private void frm_probationary_record_Load(object sender, EventArgs e)
+        {
+            loadData();
+            LoadDepartment();
+            LoadPersionalInformation();
+            bll_cn.BLL_Load_cb_pos();
+
+        }
+
+        
+        public void loadData()
+        {
+            bll_cn.BllLoadGrid();
+        }
+
+        public void LoadDepartment()
+        {
+            bll_cn.BllLoadDepartment();
+        }
+        public void LoadPersionalInformation()
+        {
+            bll_cn.BllLoadPersonalInformation();
+        }
+      
+
+
+
+
+        private void btn_refresh_Click(object sender, EventArgs e)
+        {
+            cb_pos.Text = "";
+            txt_probationary_date.Text = "";
+            txt_exprity_date.Text = "";
+            txt_note.Text = "";
+            
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            cb_pos.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            txt_probationary_date.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            txt_exprity_date.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            txt_note.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            combo_department.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+            combox_identification_number.Text = dataGridView1.CurrentRow.Cells[7].Value.ToString();
+
+
+        }
+
+        private void btn_create_Click(object sender, EventArgs e)
+        {
+            bll_cn.BllCreate();
+            loadData();
+        }
+        string check;
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+
+            check = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            bll_cn.BllUpdate(int.Parse(check));
+
+           
+
+            loadData();
+        }
+
+        private void btn_delete_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                if (MessageBox.Show("You may want to delete", "Delete data", MessageBoxButtons.YesNo, MessageBoxIcon.Warning) == DialogResult.Yes)
+                {
+                    check = dataGridView1.CurrentRow.Cells[0].Value.ToString();                   
+                    bll_cn.BllDelete(int.Parse(check));
+                    loadData();
+                }
+            }
+            catch(Exception err)
+            {
+                throw err;
+            }
+
+            loadData();
+        }
+        private void btn_exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
